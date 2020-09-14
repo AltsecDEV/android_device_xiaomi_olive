2971dca06739b2e52b391939bcd92ad09e09b609
altsec <altsec.dev@gmail.com>
2020-09-14 20:43:51 +0300

    Added system-as-root, treble support, support OTA Update

--- omni_olive.mk
+++ omni_olive.mk
@@ -22,6 +22,9 @@ $(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, build/target/product/core_64_bit.mk)
 
+#Treble Support
+$(call inherit-product, $(SRC_TARGET_DIR)/product/treble_common_64.mk)
+
 
 # Device identifier. This must come after all inclusions.
 
@@ -35,6 +38,11 @@ PRODUCT_NAME := omni_olive
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 8
 PRODUCT_MANUFACTURER := Xiaomi
+
+#Add treble compatibility
+PRODUCT_FULL_TREBLE_OVERRIDE := true 
+
+PRODUCT_PROPERTY_OVERRIDES := ro.treble.enabled=true
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
     ro.product.device \
