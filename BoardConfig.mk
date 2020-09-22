fecbd70cafc3d2a07f8c469bf0d83850135d3833
altsec <altsec.dev@gmail.com>
2020-09-22 20:18:50 +0300

    SeLinux Permissive is off, fix for custom ROMs and Other GSI ROMs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,7 +65,7 @@ BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 
 BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7824900.sdhci earlycon=msm_serial_dm,0x78B0000 firmware_class.path=/vendor/firmware_mnt/image 
 BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=true loop.max_part=7
-BOARD_KERNEL_CMDLINE += android.boot.selinux=permissive
+#BOARD_KERNEL_CMDLINE += android.boot.selinux=permissive
 
 # Kernel
 TARGET_KERNEL_ARCH := arm64
